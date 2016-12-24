ceef3a5ac59cd2e39b891e735ba8486fcc498402
Demas Sinner <demassinner@gmail.com>
2016-12-24 09:12:38 +0100

    La till ett litet tomt test

---
+++ test/savedata.js
@@ -0,0 +1,10 @@
+var chai = require('chai');
+var expect = chai.expect; // we are using the "expect" style of Chai
+
+describe('hip hop', function() {
+  
+  it('sdadd', function() {
+    expect(true).to.be.true;
+  });
+
+});
